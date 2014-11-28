642d514081883c5611815fabd25c7ab7af4dfc72
Vasil Vanchuk <v.vanchuk@tut.by>
2014-11-28 21:21:27 +0300

    Move rote definition to annotation

--- src/Shop/WebSiteBundle/Controller/IndexController.php
+++ src/Shop/WebSiteBundle/Controller/IndexController.php
@@ -3,9 +3,13 @@
 namespace Shop\WebSiteBundle\Controller;
 
 use Shop\CommonBundle\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class IndexController extends Controller\AbstractController
 {
+    /**
+     * @Route("/")
+     */
     public function indexAction()
     {
         return $this->render('WebSiteBundle:Index:index.html.twig');
