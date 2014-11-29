c83b7be2f08731c2eec760453f06de98fce29733
Vasil Vanchuk <v.vanchuk@tut.by>
2014-11-29 22:40:20 +0300

    Implement render method for common controller

--- src/Shop/CommonBundle/Controller/CommonController.php
+++ src/Shop/CommonBundle/Controller/CommonController.php
@@ -2,9 +2,38 @@
 
 namespace Shop\CommonBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
-abstract class CommonController extends Controller
+class CommonController
 {
+    protected $tplLoader = NULL;
 
+    public function __construct()
+    {
+        $this->setLoader();
+    }
+
+    protected function setLoader()
+    {
+        $this->tplLoader = new \Twig_Loader_Filesystem(array(
+                '../src/Shop/WebSiteBundle/Resources/views',
+                '../src/Shop/CommonBundle/Resources',
+                '../src/Shop'
+            )
+        );
+
+        list($currentBundleName) = explode('Controller', get_called_class());
+        $currentBundleName = str_replace('\\', '/', $currentBundleName);
+        $currentBundleViewsPath = '../src/' . $currentBundleName . 'Resources/views';
+
+        $this->tplLoader->addPath($currentBundleViewsPath);
+    }
+
+
+    public function render($view, $parameters = array())
+    {
+        $twig = new \Twig_Environment($this->tplLoader);
+
+        return new Response($twig->render($view, $parameters));
+    }
 }
