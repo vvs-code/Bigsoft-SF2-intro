1ef1b0257710b0c9662f600faf44c37e5d4efad0
Vasil Vanchuk <v.vanchuk@tut.by>
2014-12-02 23:19:35 +0300

    Pass templating engine to CommonController

--- src/Shop/CommonBundle/Controller/CommonController.php
+++ src/Shop/CommonBundle/Controller/CommonController.php
@@ -3,38 +3,18 @@
 namespace Shop\CommonBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
+use Symfony\Bundle\TwigBundle\TwigEngine;
 
 class CommonController
 {
     protected $templating;
 
-    public function __construct(EngineInterface $templating)
-    {
+    public function setTemplating(TwigEngine $templating){
         $this->templating = $templating;
     }
 
-    protected function setLoader()
-    {
-        $this->tplLoader = new \Twig_Loader_Filesystem(array(
-                '../src/Shop/WebSiteBundle/Resources/views',
-                '../src/Shop/CommonBundle/Resources',
-                '../src/Shop'
-            )
-        );
-
-        list($currentBundleName) = explode('Controller', get_called_class());
-        $currentBundleName = str_replace('\\', '/', $currentBundleName);
-        $currentBundleViewsPath = '../src/' . $currentBundleName . 'Resources/views';
-
-        $this->tplLoader->addPath($currentBundleViewsPath);
-    }
-
-
     public function render($view, $parameters = array())
     {
-        $twig = new \Twig_Environment($this->tplLoader);
-
-        return new Response($twig->render($view, $parameters));
+        return new Response($this->templating->render($view, $parameters));
     }
 }
--- src/Shop/WebSiteBundle/Controller/IndexController.php
+++ src/Shop/WebSiteBundle/Controller/IndexController.php
@@ -4,6 +4,9 @@ namespace Shop\WebSiteBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
+/**
+ * @Route(service="shop.website.index_controller")
+ */
 class IndexController extends \Shop\CommonBundle\Controller\CommonController
 {
     /**
@@ -11,6 +14,6 @@ class IndexController extends \Shop\CommonBundle\Controller\CommonController
      */
     public function indexAction()
     {
-        return $this->render('Index/index.html.twig');
+        return $this->render('WebSiteBundle:Index:index.html.twig');
     }
 }
