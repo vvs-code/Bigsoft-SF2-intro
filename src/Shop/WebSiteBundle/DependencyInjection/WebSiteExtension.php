fb2755bdd5ae01ef8071231b3f37e09d822c2532
Vasil Vanchuk <v.vanchuk@tut.by>
2014-12-03 17:14:22 +0300

    Fix configuration loading

--- src/Shop/WebSiteBundle/DependencyInjection/WebSiteExtension.php
+++ src/Shop/WebSiteBundle/DependencyInjection/WebSiteExtension.php
@@ -23,5 +23,6 @@ class WebSiteExtension extends Extension
         $config = $this->processConfiguration($configuration, $configs);
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader->load('config.yml');
     }
 }
