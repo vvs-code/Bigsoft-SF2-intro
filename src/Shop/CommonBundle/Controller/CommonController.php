513638de6f082b20ca109a11d5df3042dc4f12e5
Vasil Vanchuk <vasil.vanchuk@strevus.com>
2014-12-03 16:47:39 +0300

    Add docBlock for CommonController

--- src/Shop/CommonBundle/Controller/CommonController.php
+++ src/Shop/CommonBundle/Controller/CommonController.php
@@ -2,20 +2,39 @@
 
 namespace Shop\CommonBundle\Controller;
 
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\TwigBundle\TwigEngine;
 
+
+/**
+ * Class CommonController
+ * @package Shop\CommonBundle\Controller
+ */
 class CommonController
 {
+    /**
+     * @var TwigEngine Should contain templating-engine instance
+     */
     protected $templating;
 
+    /**
+     * Set templating engine
+     * @param TwigEngine $templating
+     * @return CommonController this
+     */
     public function setTemplating(TwigEngine $templating)
     {
         $this->templating = $templating;
+        return $this;
     }
 
+    /**
+     * Render view and return Response
+     * @param string $view view name to render
+     * @param array $parameters parametrs passed to view
+     * @return Response
+     */
     public function render($view, $parameters = array())
     {
-        return new Response($this->templating->render($view, $parameters));
+        return $this->templating->renderResponse($view, $parameters);
     }
 }
