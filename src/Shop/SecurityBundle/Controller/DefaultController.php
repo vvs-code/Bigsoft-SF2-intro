27baa2a5251817b992c937b2acefc4ec245ec441
Vasil Vanchuk <v.vanchuk@tut.by>
2014-12-10 08:31:22 -0800

    Use new array syntaxis

--- src/Shop/SecurityBundle/Controller/DefaultController.php
+++ src/Shop/SecurityBundle/Controller/DefaultController.php
@@ -26,11 +26,11 @@ class DefaultController extends Controller\CommonController
             $error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
         }
 
-        return $this->render('ShopSecurityBundle:Default:login.html.twig', array(
+        return $this->render('ShopSecurityBundle:Default:login.html.twig', [
             // имя, введённое пользователем в последний раз
             'last_username' => $session->get(SecurityContext::LAST_USERNAME),
             'error'         => $error,
-        ));
+        ]);
     }
 }
 
