d752351d397e7ce912de34cc7488d2bd8738f135
Vasil Vanchuk <v.vanchuk@tut.by>
2014-12-12 10:25:54 -0800

    Add doctrine migrations to composer dependencies

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
+            new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
             new Shop\CommonBundle\CommonBundle(),
             new Shop\WebSiteBundle\WebSiteBundle(),
             new Shop\AdminBundle\AdminBundle(),
