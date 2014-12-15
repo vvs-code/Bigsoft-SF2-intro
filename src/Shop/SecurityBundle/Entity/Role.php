2159801c92cd648b27d77d6df48a5792a475f83c
Vasil Vanchuk <v.vanchuk@tut.by>
2014-12-15 08:21:24 -0800

    Use inheritDoc for getRole

--- src/Shop/SecurityBundle/Entity/Role.php
+++ src/Shop/SecurityBundle/Entity/Role.php
@@ -35,14 +35,7 @@ class Role implements RoleInterface, \Serializable
     }
 
     /**
-     * Returns the role.
-     *
-     * This method returns a string representation whenever possible.
-     *
-     * When the role cannot be represented with sufficient precision by a
-     * string, it should return null.
-     *
-     * @return string|null A string representation of the role, or null
+     * @inheritDoc
      */
     public function getRole()
     {
