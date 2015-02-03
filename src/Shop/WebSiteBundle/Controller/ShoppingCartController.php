34aecb931c25ed89fe42073b33b9f6e419767c01
Vasil Vanchuk <v.vanchuk@tut.by>
2015-02-03 14:52:02 -0800

    Add shopping cart controller

---
+++ src/Shop/WebSiteBundle/Controller/ShoppingCartController.php
@@ -0,0 +1,35 @@
+<?php
+namespace Shop\WebSiteBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Shop\CommonBundle\Controller;
+use Shop\WebSiteBundle\Entity\Product;
+use Shop\WebSiteBundle\Service\ShoppingCartService;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * @Route("/cart", service="shop.website.shopping_cart_controller")
+ */
+class ShoppingCartController extends Controller\CommonController
+{
+    /**
+     * @var ShoppingCartService
+     */
+    private $shoppingCartService;
+
+    public function setShoppingCartService(ShoppingCartService $shoppingCartService) {
+        $this->shoppingCartService = $shoppingCartService;
+    }
+
+    /**
+     * @Route("/list", name="shopping_cart_list")
+     * @Template()
+     */
+    public function listAction(Request $request)
+    {
+        return [
+            'products' => $this->shoppingCartService->getCartProducts()
+        ];
+    }
+}
