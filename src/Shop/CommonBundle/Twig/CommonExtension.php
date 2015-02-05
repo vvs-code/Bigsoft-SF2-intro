2ada302111d76a00e50b692bad7e92b45db6521b
Vasil Vanchuk <v.vanchuk@tut.by>
2015-02-05 12:21:16 -0800

    Add empty form twig extension

---
+++ src/Shop/CommonBundle/Twig/CommonExtension.php
@@ -0,0 +1,51 @@
+<?php
+namespace Shop\CommonBundle\Twig;
+
+use Symfony\Component\Form\FormFactory;
+use Shop\CommonBundle\Form\EmptyFormType;
+
+class CommonExtension extends \Twig_Extension
+{
+    /**
+     * @var FormFactory
+     */
+    protected $formFactory;
+
+    /**
+     * @param FormFactory $ff
+     */
+    public function setFormFactory(FormFactory $ff)
+    {
+        $this->formFactory = $ff;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getFunctions() {
+        return array(
+            'empty_post_form' => new \Twig_Function_Method($this, 'getEmptyPostForm')
+        );
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmptyPostForm($route = '', array $options = []) {
+        if(!isset($options['action'])){
+            $options['action'] = $route;
+        }
+
+        return $this->formFactory
+            ->createBuilder(new EmptyFormType, $options)
+            ->getForm()
+            ->createView();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName() {
+        return 'common_extension';
+    }
+}
